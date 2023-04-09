64025f6181feefd2ed991e3250070c1d7cc203ac
muballighhossain <mubahossain99@gmail.com>
2023-04-09 22:56:52 +0600

    method overloading

--- Part10.py
+++ Part10.py
@@ -1,1 +1,28 @@
-class my_calculator
+class Student:
+    # def __init__(self, name, Id):
+    #     self.name = name
+    #     self.id = Id
+
+    # def __init__(self, name, Id, cg):
+    #     self.name = name
+    #     self.id = Id
+    #     self.cg = cg
+
+    def __init__(self, *info):
+        if len(info) == 3:
+            self.name = info[0]
+            self.id = info[1]
+            self.cg = info[2]
+        elif len(info) == 2:
+            self.name = info[0]
+            self.id = info[1]
+        elif len(info) == 1:
+            self.name = info[0]
+
+        print("A student object is created")
+
+
+s1 = Student("Carol", 33, 3.95)
+s2 = Student("bob", 11)
+s3 = Student("mub")
+s4 = Student()
